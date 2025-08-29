c6ff841a64f7841e9bf3f6f679c364a93582c07e
Jose <josedelvalle012@gmail.com>
2025-08-29 10:41:39 -0300

    Feat(header): add header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Navbar from "../Navbar/Navbar";
+
+function Header() {
+    return (
+        <div className="relative bg-gray-950 h-screen w-full">
+                <Navbar></Navbar>
+
+            <div className="absolute inset-0">
+                <img
+                src="/src/assets/UsoPerfume2.png"
+                alt="Perfume Background"
+                className="w-full h-full object-cover opacity-20"></img>
+            </div>
+            <div className="absolute inset-0">
+                <video
+                src="/Efecto HUMO para tus videos.mp4"
+                autoPlay
+                muted
+                loop
+                playsInline
+                className="w-full h-full object-cover opacity-80 mix-blend-screen"></video>
+            </div>
+            
+            <div className="relative z-10 flex items-center justify-center h-full text-center">
+                <h1 className="font-roboto(Almarai) text-white font-normal text-6xl">
+                    Fragrance for people who want more style and mood reflection.
+                </h1>
+            </div>
+            </div>
+        
+    )
+}
+
+export default Header;
