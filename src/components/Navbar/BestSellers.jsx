fbc05709dcd35b5712824c737dc4f2a6065154d4
Jose <josedelvalle012@gmail.com>
2025-09-05 10:18:10 -0300

    feature(best-sellers): add cards menu above the navbar

--- src/components/Navbar/BestSellers.jsx
+++ src/components/Navbar/BestSellers.jsx
@@ -4,18 +4,25 @@ function BestSellers({isOpen}) {
     if (!isOpen) return null;
 
     const perfumes = [
-        { id: 1, name: "Perfume 1", image: "/src/assets/Lattafa/Lattafa_Fakhar.png" },
-        { id: 2, name: "Perfume 2", image: "/src/assets/AlHaramain/AlHaramain_UrbanistFemme.png" },
-        { id: 3, name: "Perfume 3", image: "/src/assets/Armaf/Armaf_Blue-Homme.png" },
+        { id: 1, name: "Lattafa", image: "/src/assets/Lattafa/Lattafa_Fakhar.png" },
+        { id: 2, name: "AlHaramain", image: "/src/assets/AlHaramain/AlHaramain_UrbanistFemme.png" },
+        { id: 3, name: "Armaf", image: "/src/assets/Armaf/Armaf_Blue-Homme.png" },
     ];
     return (
         <div 
-        className="absolute top-16 left-1/2 -translate-x-1/2 bg-white rounded-lg shadow-lg p-4 flex gap-4 z-50"
+        className="top-full left-0 w-screen bg-white shadow-lg py-6 z-40"
         onMouseEnter={(e) => e.stopPropagation()}
         >
-        {perfumes.map((p) => (
-            <PerfumeCard id={p.id} name={p.name} image={p.image}></PerfumeCard>
-        ))}
+        <div className="max-w-4/5 mx-auto grid grid-cols-3">
+            {perfumes.map((p) => (
+                <PerfumeCard id={p.id} 
+                name={p.name} 
+                image={p.image} 
+                isActive={true} 
+                className="shadow-lg shadow-gray-400" 
+                classNameImg="object-cover transition-transform duration-300 ease-in-out hover:scale-110"></PerfumeCard>
+            ))}
+        </div>
     </div>
     )
 }
